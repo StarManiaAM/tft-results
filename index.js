01b6f5b3244761a272c302677e4cb90043de906d
Amory Marchal <amory.marchal@epita.fr>
2025-09-03 23:22:19 +0200

    feat(client): Connect client

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv/config');
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages
+    ] });
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    // Use wanted ChannelID
+    const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+    if (channel) {
+        channel.send("Hello! I'm online 🚀");
+    } else {
+        console.error("Channel not found. Did you use the correct ID?");
+    }
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.DISCORD_CLIENT_TOKEN)
