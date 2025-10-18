0ba5284793518569b7d1aa383af0d5ff63c2fb93
Amory Marchal <marchal.amory@gmail.com>
2025-10-18 22:50:02 +0200

    feat: es linter security

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 import js from "@eslint/js";
 import globals from "globals";
 import {defineConfig} from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
     {
         files: ["**/*.{js,mjs,cjs}"],
-        plugins: {js},
-        extends: ["js/recommended"],
+        plugins: {js, pluginSecurity},
+        extends: ["js/recommended", pluginSecurity.configs.recommended],
         languageOptions: {globals: globals.node},
     }
 ]);
